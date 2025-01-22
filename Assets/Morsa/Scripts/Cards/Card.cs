ce682ce2685f16ecc7e694f660a0796b828ebc39
Morsa <167016181+morsaastuta@users.noreply.github.com>
2025-01-22 13:55:00 +0100

    fixcard

--- Assets/Morsa/Scripts/Cards/Card.cs
+++ Assets/Morsa/Scripts/Cards/Card.cs
@@ -1,3 +1,4 @@
+using Glossary;
 using System.Collections.Generic;
 using UnityEngine;
 
--- Assets/Morsa/Scripts/Glossary.cs
+++ Assets/Morsa/Scripts/Glossary.cs
@@ -4,6 +4,10 @@ namespace Glossary
 {
     public enum Property
     {
+        // The Fool
+        Innocence, Spontaneity, Risk, Fearlessness,
 
+        // The Magician
+        Potential, Desire, Shame
     }
 }
